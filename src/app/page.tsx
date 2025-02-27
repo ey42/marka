c5be94d0b3344d9ab5d39ed0dc83e9a3640244c1
unknown <ey42>
2025-02-27 12:32:49 -0900

    nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Landing from '@/Component/catagory/Landing';
-import { db } from '@/drizzle'
-import * as schema from "@/drizzle/db/schema"
+
 
 export default async function Home() {
- const AllCatagory:Catagoryprops[] =  await db.select().from(schema.catagories)
   return (
     <div className= "dark:text-light text-dark mt-4 h-full flex gap-2 flex-wrap justify-evenly items-start w-full">
-     <Landing AllCatagory={AllCatagory}/>
+     <Landing/>
     </div>
   );
 }
